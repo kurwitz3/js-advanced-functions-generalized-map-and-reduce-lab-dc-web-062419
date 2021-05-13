f0fdb5ae85305b125de8d4f85d9077508b49fcce
kurwitz3 <kurwitz3@aol.com>
2021-05-13 18:24:18 -0400

    commit tree 1b0f0a7f9af098e93e31ea09a120ee9116eb4304

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+function map(sourceArray,func){
+ return sourceArray.map(number => func(number))
+}
+
+function reduce (array, func, start) {
+    let value;
+ 
+    if(start) {
+        value = start;
+        for (let i = 0; i < array.length; i++) {
+         return func(array[i], value);
+        }
+       
+    }
+ 
+    else {
+        value = array[0];
+        for (let i = 1; i < array.length; i++) {
+            value = func(array[i], value);
+        }
+    }
+    return value;
+ }
